b51790ba7eb7395641acfb781a84dbde819bcda8
OrlineSorel <sorelleketcha@gmail.com>
2020-08-03 10:12:50 +0200

    Added date_parser function into the script

--- team_3_script.py
+++ team_3_script.py
@@ -28,3 +28,18 @@ def word_splitter(df):
 
 
 
+def date_parser(dates):
+
+    ''' This function takes in a string consisting of a date and time
+    in the form 'yyyy-mm-dd hh:mm:ss and returns just the date in
+    the form 'yyy-mm-dd'.
+    '''
+
+    newList = []  # creating an empty list
+    for i in dates:  # for loop
+        newList.append(i[:10])  # choosing the first 10 letters in 'dates'
+    return newList
+
+
+
+
