4f5e0197da2b67824662efb822fb4dd6e268c59b
Warren-Mbekezeli-Mnisi <45631355+Warren-Mbekezeli-Mnisi@users.noreply.github.com>
2020-08-07 09:55:32 +0200

    Fixing double assignment in dictionary of metrics

--- team_3_analyse_package/team_3_analyse_package/analyse.py
+++ team_3_analyse_package/team_3_analyse_package/analyse.py
@@ -61,37 +61,32 @@ def dictionary_of_metrics(items):
          and 'max', corresponding to the mean, median, standard deviation, 
          variance, minimum and maximum of the input list, respectively.
     """
-    metrics = {}
+
+    metrics={}
 
     # Mean
-    mean = round(np.mean(items), 2)
-    mean = {'mean': mean}
-    metrics.update(mean)
+    mean = round(np.mean(items),2)
+    metrics['mean'] = mean
 
     # median
-    median = round(np.median(items), 2)
-    median = {'median': median}
-    metrics.update(median)
+    median = round(np.median(items),2)
+    metrics['median'] = median
 
     # variance
-    var = round(np.var(items, ddof=1), 2)
-    var = {'var': var}
-    metrics.update(var)
+    var = round(np.var(items,ddof=1),2)
+    metrics['var'] = var
 
     # std
     std = round(np.std(items, ddof=1), 2)
-    std = {'std': std}
-    metrics.update(std)
+    metrics['std'] = std       
 
     # min
-    mini = round(min(items), 2)
-    min_d = {'min': mini}
-    metrics.update(min_d)
+    mini=round(min(items),2)      
+    metrics['min'] = mini
 
     # max
-    maxi = round(max(items), 2)
-    max_d = {'max': maxi}
-    metrics.update(max_d)
+    maxi=round(max(items),2)
+    metrics['max'] = maxi
 
     return metrics
 
