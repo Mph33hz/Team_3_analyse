fb846a272ea95f3164d55f8c107ae2cb47a48a39
Mpho Mokhokane <49303929+Mph33hz@users.noreply.github.com>
2020-08-10 11:23:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     description ='Analyse predict for team 3',
     long_description = open('readme.MD').read(),
     install_requires = ['numpy', 'pandas'],
-    url = 'https://github.com/Mph33hz/Team_3_analyse.git'
-    author = 'Mpho Mokhokane'
-    author_email = 'Mphomokhokane@gmail.com'
-    Co-authored-by = 'Kgauhelo Mokgawa' <mokgawakgauhelo@gmail.com>
-    Co-authored-by = 'Sorel Ketcha' <sorelleketcha@gmail.com>
-    Co-authored-by = 'Warren Mnisi' <mbekezelimnisi@gmail.com>
+    url = 'https://github.com/Mph33hz/Team_3_analyse.git',
+    author = 'Mpho Mokhokane',
+    author_email = 'Mphomokhokane@gmail.com',
+    Co-authored-by = 'Kgauhelo Mokgawa' <mokgawakgauhelo@gmail.com>,
+    Co-authored-by = 'Sorel Ketcha' <sorelleketcha@gmail.com>,
+    Co-authored-by = 'Warren Mnisi' <mbekezelimnisi@gmail.com>,
     Co-authored-by = 'Lazola Javu' <lazola.javu@gmail.com>
 )
