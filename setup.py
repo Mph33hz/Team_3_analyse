45c4ee2fe2ef9efc2fc881bf71c4f576d82b9523
LazolaJavu <42202425+LazolaJavu@users.noreply.github.com>
2020-08-10 11:42:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires = ['numpy', 'pandas'],
     url = 'https://github.com/Mph33hz/Team_3_analyse.git',
     author = 'Mpho Mokhokane, Kgauhelo Mokgawa  , Sorel Ketcha , Warren Mnisi , Lazola Javu',
-    author_email : 'Mphomokhokane@gmail.com, mokgawakgauhelo@gmail.com, sorelleketcha@gmail.com, mbekezelimnisi@gmail.com, lazola.javu@gmail.com'
+    author_email : 'Mphomokhokane@gmail.com, mokgawakgauhelo@gmail.com, sorelleketcha@gmail.com, mbekezelimnisi@gmail.com, lazola.javu@gmail.com',
 #     Co-authored-by : 'Kgauhelo Mokgawa' <mokgawakgauhelo@gmail.com>
 #     Co-authored-by : 'Sorel Ketcha' <sorelleketcha@gmail.com>
 #     Co-authored-by : 'Warren Mnisi' <mbekezelimnisi@gmail.com>
