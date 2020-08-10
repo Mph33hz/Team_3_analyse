a6febee600120bc6305929cda57d71da1274dc88
Mpho Mokhokane <49303929+Mph33hz@users.noreply.github.com>
2020-08-10 11:34:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(
     long_description = open('readme.MD').read(),
     install_requires = ['numpy', 'pandas'],
     url = 'https://github.com/Mph33hz/Team_3_analyse.git',
-    author = 'Mpho Mokhokane'
-    author_email : 'Mphomokhokane@gmail.com',
-    Co-authored-by : 'Kgauhelo Mokgawa' <mokgawakgauhelo@gmail.com>
-    Co-authored-by : 'Sorel Ketcha' <sorelleketcha@gmail.com>
-    Co-authored-by : 'Warren Mnisi' <mbekezelimnisi@gmail.com>
-    Co-authored-by : 'Lazola Javu' <lazola.javu@gmail.com>
+    author = 'Mpho Mokhokane, Kgauhelo Mokgawa  , Sorel Ketcha , Warren Mnisi , Lazola Javu',
+    author_email : 'Mphomokhokane@gmail.com, mokgawakgauhelo@gmail.com, sorelleketcha@gmail.com, mbekezelimnisi@gmail.com, lazola.javu@gmail.com'
+#     Co-authored-by : 'Kgauhelo Mokgawa' <mokgawakgauhelo@gmail.com>
+#     Co-authored-by : 'Sorel Ketcha' <sorelleketcha@gmail.com>
+#     Co-authored-by : 'Warren Mnisi' <mbekezelimnisi@gmail.com>
+#     Co-authored-by : 'Lazola Javu' <lazola.javu@gmail.com>
 )
